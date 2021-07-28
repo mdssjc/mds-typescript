8aa344ae2dce73e9f2b8b59ec79abb081804fe6b
Marcelo dos Santos <mdssjc@gmail.com>
2021-07-27 21:39:46 -0300

    feat: add Adding an Input

--- udemy/the-modern-angular-bootcamp/section-5-mastering-pipes/pipes/src/app/app.component.ts
+++ udemy/the-modern-angular-bootcamp/section-5-mastering-pipes/pipes/src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string = '';
+
+  onNameChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.name = target.value;
+  }
 }
