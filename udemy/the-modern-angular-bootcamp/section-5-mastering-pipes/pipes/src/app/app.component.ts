7480fc6e49cd02a2822d1e8bea81983fa7c67b97
Marcelo dos Santos <mdssjc@gmail.com>
2021-07-28 22:14:24 -0300

    feat: add Formatting Numbers

--- udemy/the-modern-angular-bootcamp/section-5-mastering-pipes/pipes/src/app/app.component.ts
+++ udemy/the-modern-angular-bootcamp/section-5-mastering-pipes/pipes/src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
   name: string = '';
   date: string = '';
   amount: number = 0.0;
+  height: number = 0.0;
 
   onNameChange(event: Event) {
     const target = event.target as HTMLInputElement;
@@ -23,5 +24,10 @@ export class AppComponent {
   onAmountChange(event: Event) {
     const target = event.target as HTMLInputElement;
     this.amount = parseFloat(target.value);
+  }
+
+  onHeightChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.height = parseFloat(target.value);
   }
 }
