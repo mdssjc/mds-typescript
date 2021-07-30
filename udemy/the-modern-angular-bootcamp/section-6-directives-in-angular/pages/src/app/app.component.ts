2922134a039f29a397176e807e6d2550857f3fff
Marcelo dos Santos <mdssjc@gmail.com>
2021-07-29 21:53:55 -0300

    feat: add Getting Some Data

--- udemy/the-modern-angular-bootcamp/section-6-directives-in-angular/pages/src/app/app.component.ts
+++ udemy/the-modern-angular-bootcamp/section-6-directives-in-angular/pages/src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pages';
+  images = [
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    },
+  ];
 }
