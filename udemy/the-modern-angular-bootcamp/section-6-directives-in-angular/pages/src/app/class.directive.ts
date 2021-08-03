fe04e42e4a480327698fcecca32a3ecb46d552d0
Marcelo dos Santos <mdssjc@gmail.com>
2021-08-02 22:46:38 -0300

    feat: add Intercepting a Property Assignment

--- udemy/the-modern-angular-bootcamp/section-6-directives-in-angular/pages/src/app/class.directive.ts
+++ udemy/the-modern-angular-bootcamp/section-6-directives-in-angular/pages/src/app/class.directive.ts
@@ -4,9 +4,9 @@ import { Directive, ElementRef, Input } from '@angular/core';
   selector: '[appClass]',
 })
 export class ClassDirective {
-  @Input() backgroundColor: string;
+  constructor(private element: ElementRef) {}
 
-  constructor(private element: ElementRef) {
-    this.element.nativeElement.style.backgroundColor = this.backgroundColor;
+  @Input() set backgroundColor(color: string) {
+    this.element.nativeElement.style.backgroundColor = color;
   }
 }
