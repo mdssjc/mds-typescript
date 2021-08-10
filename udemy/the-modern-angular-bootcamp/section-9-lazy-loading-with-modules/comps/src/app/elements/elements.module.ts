6a53904ebbb5111873971f2e7e5be711a491d5ff
Marcelo dos Santos <mdssjc@gmail.com>
2021-08-09 22:45:24 -0300

    feat: add Creating a Placeholder Component

--- udemy/the-modern-angular-bootcamp/section-9-lazy-loading-with-modules/comps/src/app/elements/elements.module.ts
+++ udemy/the-modern-angular-bootcamp/section-9-lazy-loading-with-modules/comps/src/app/elements/elements.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { ElementsRoutingModule } from './elements-routing.module';
+import { PlaceholderComponent } from './placeholder/placeholder.component';
 
 @NgModule({
-  declarations: [ElementsHomeComponent],
+  declarations: [ElementsHomeComponent, PlaceholderComponent],
   imports: [CommonModule, ElementsRoutingModule],
 })
 export class ElementsModule {}
