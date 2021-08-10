675aca898e0a474eb2a20f75ecf2a5c9e7fdee00
Marcelo dos Santos <mdssjc@gmail.com>
2021-08-09 22:56:31 -0300

    feat: add Reminder on Structural Directives

--- udemy/the-modern-angular-bootcamp/section-9-lazy-loading-with-modules/comps/src/app/elements/elements.module.ts
+++ udemy/the-modern-angular-bootcamp/section-9-lazy-loading-with-modules/comps/src/app/elements/elements.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { ElementsRoutingModule } from './elements-routing.module';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
+import { TimesDirective } from './times.directive';
 
 @NgModule({
-  declarations: [ElementsHomeComponent, PlaceholderComponent],
+  declarations: [ElementsHomeComponent, PlaceholderComponent, TimesDirective],
   imports: [CommonModule, ElementsRoutingModule],
 })
 export class ElementsModule {}
