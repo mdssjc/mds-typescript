762e03384eda9bc9adec03b3dbcd040bbc05f1a3
Marcelo dos Santos <mdssjc@gmail.com>
2021-08-11 22:16:50 -0300

    feat: add Creating a Segment Component

--- udemy/the-modern-angular-bootcamp/section-9-lazy-loading-with-modules/comps/src/app/elements/elements.module.ts
+++ udemy/the-modern-angular-bootcamp/section-9-lazy-loading-with-modules/comps/src/app/elements/elements.module.ts
@@ -5,9 +5,10 @@ import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { ElementsRoutingModule } from './elements-routing.module';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
 import { TimesDirective } from './times.directive';
+import { SegmentComponent } from './segment/segment.component';
 
 @NgModule({
-  declarations: [ElementsHomeComponent, PlaceholderComponent, TimesDirective],
+  declarations: [ElementsHomeComponent, PlaceholderComponent, TimesDirective, SegmentComponent],
   imports: [CommonModule, ElementsRoutingModule, SharedModule],
 })
 export class ElementsModule {}
