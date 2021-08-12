2adc43af4292e80f0427d5d519fbe63632deaa36
Marcelo dos Santos <mdssjc@gmail.com>
2021-08-11 22:07:00 -0300

    feat: add Implementing a Titled Divider

--- udemy/the-modern-angular-bootcamp/section-9-lazy-loading-with-modules/comps/src/app/elements/elements.module.ts
+++ udemy/the-modern-angular-bootcamp/section-9-lazy-loading-with-modules/comps/src/app/elements/elements.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { ElementsRoutingModule } from './elements-routing.module';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
@@ -7,6 +8,6 @@ import { TimesDirective } from './times.directive';
 
 @NgModule({
   declarations: [ElementsHomeComponent, PlaceholderComponent, TimesDirective],
-  imports: [CommonModule, ElementsRoutingModule],
+  imports: [CommonModule, ElementsRoutingModule, SharedModule],
 })
 export class ElementsModule {}
