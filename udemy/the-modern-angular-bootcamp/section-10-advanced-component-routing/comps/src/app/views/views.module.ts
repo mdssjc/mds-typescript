7f6f932c7de866dd52eeec680271b6c77324b2c2
Marcelo dos Santos <mdssjc@gmail.com>
2021-08-23 22:57:39 -0300

    feat: add Displaying a List of Items

--- udemy/the-modern-angular-bootcamp/section-10-advanced-component-routing/comps/src/app/views/views.module.ts
+++ udemy/the-modern-angular-bootcamp/section-10-advanced-component-routing/comps/src/app/views/views.module.ts
@@ -4,9 +4,10 @@ import { SharedModule } from '../shared/shared.module';
 import { StatisticsComponent } from './statistics/statistics.component';
 import { ViewsHomeComponent } from './views-home/views-home.component';
 import { ViewsRoutingModule } from './views-routing.module';
+import { ItemListComponent } from './item-list/item-list.component';
 
 @NgModule({
-  declarations: [ViewsHomeComponent, StatisticsComponent],
+  declarations: [ViewsHomeComponent, StatisticsComponent, ItemListComponent],
   imports: [CommonModule, ViewsRoutingModule, SharedModule],
 })
 export class ViewsModule {}
