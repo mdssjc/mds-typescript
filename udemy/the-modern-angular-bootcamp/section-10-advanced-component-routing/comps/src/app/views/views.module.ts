e96ccaadca69a608c7eaf511355d78af458879bb
Marcelo dos Santos <mdssjc@gmail.com>
2021-08-23 22:31:53 -0300

    feat: add Views Module Exercise

--- udemy/the-modern-angular-bootcamp/section-10-advanced-component-routing/comps/src/app/views/views.module.ts
+++ udemy/the-modern-angular-bootcamp/section-10-advanced-component-routing/comps/src/app/views/views.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ViewsRoutingModule } from './views-routing.module';
+import { ViewsComponent } from './views.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { ViewsHomeComponent } from './views-home/views-home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ViewsComponent,
+    StatisticsComponent,
+    ViewsHomeComponent
+  ],
   imports: [
     CommonModule,
     ViewsRoutingModule
