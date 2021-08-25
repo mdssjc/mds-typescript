e65f8c31097c6c8a21d418f63bc6692b137eab64
Marcelo dos Santos <mdssjc@gmail.com>
2021-08-24 22:56:31 -0300

    feat: add Modal Window Setup

--- udemy/the-modern-angular-bootcamp/section-11-advanced-component-reusability-hooks/comps/src/app/mods/mods.module.ts
+++ udemy/the-modern-angular-bootcamp/section-11-advanced-component-reusability-hooks/comps/src/app/mods/mods.module.ts
@@ -1,21 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { ModsRoutingModule } from './mods-routing.module';
-import { ModsComponent } from './mods.component';
-import { ModsHomeComponent } from './mods-home/mods-home.component';
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
 import { ModalComponent } from './modal/modal.component';
-
+import { ModsHomeComponent } from './mods-home/mods-home.component';
+import { ModsRoutingModule } from './mods-routing.module';
 
 @NgModule({
-  declarations: [
-    ModsComponent,
-    ModsHomeComponent,
-    ModalComponent
-  ],
-  imports: [
-    CommonModule,
-    ModsRoutingModule
-  ]
+  declarations: [ModsHomeComponent, ModalComponent],
+  imports: [CommonModule, ModsRoutingModule, SharedModule],
 })
-export class ModsModule { }
+export class ModsModule {}
