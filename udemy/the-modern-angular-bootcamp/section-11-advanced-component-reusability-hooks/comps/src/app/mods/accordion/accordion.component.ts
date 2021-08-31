fe442fbe018f4d37a9012dee8ed1f17155cb1fda
Marcelo dos Santos <mdssjc@gmail.com>
2021-08-30 22:47:40 -0300

    feat: add Listing Accordion Elements

--- udemy/the-modern-angular-bootcamp/section-11-advanced-component-reusability-hooks/comps/src/app/mods/accordion/accordion.component.ts
+++ udemy/the-modern-angular-bootcamp/section-11-advanced-component-reusability-hooks/comps/src/app/mods/accordion/accordion.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-accordion',
   templateUrl: './accordion.component.html',
-  styleUrls: ['./accordion.component.css']
+  styleUrls: ['./accordion.component.css'],
 })
 export class AccordionComponent implements OnInit {
+  @Input() items = [] as any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
