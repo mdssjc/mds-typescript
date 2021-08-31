ad258bce8b1b1aff0605a251e761f1d459a2deac
Marcelo dos Santos <mdssjc@gmail.com>
2021-08-30 22:37:39 -0300

    feat: add Building an Accordion

--- udemy/the-modern-angular-bootcamp/section-11-advanced-component-reusability-hooks/comps/src/app/mods/mods.module.ts
+++ udemy/the-modern-angular-bootcamp/section-11-advanced-component-reusability-hooks/comps/src/app/mods/mods.module.ts
@@ -4,9 +4,10 @@ import { SharedModule } from '../shared/shared.module';
 import { ModalComponent } from './modal/modal.component';
 import { ModsHomeComponent } from './mods-home/mods-home.component';
 import { ModsRoutingModule } from './mods-routing.module';
+import { AccordionComponent } from './accordion/accordion.component';
 
 @NgModule({
-  declarations: [ModsHomeComponent, ModalComponent],
+  declarations: [ModsHomeComponent, ModalComponent, AccordionComponent],
   imports: [CommonModule, ModsRoutingModule, SharedModule],
 })
 export class ModsModule {}
