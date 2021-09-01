50c26079ae8d4728602ccce30f21cb2c2ef08b37
Marcelo dos Santos <mdssjc@gmail.com>
2021-08-31 22:53:24 -0300

    feat: add Basic Types

---
+++ udemy/the-modern-angular-bootcamp/section-12-a-typescript-overview/prod/ts/index.ts
@@ -0,0 +1,6 @@
+const myName: string = "Marcelo";
+const myAge: number = 37;
+const yes: boolean = true;
+const anyThing: any = { op: "a", op2: "b" };
+const nothingHere: null = null;
+const noDefinition: undefined = undefined;
