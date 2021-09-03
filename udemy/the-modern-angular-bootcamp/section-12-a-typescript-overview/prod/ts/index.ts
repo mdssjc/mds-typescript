2d27fdb8c4ded672e6a96d700f77efa73daf6c3c
Marcelo dos Santos <mdssjc@gmail.com>
2021-09-02 22:35:03 -0300

    feat: add TS with Functions

--- udemy/the-modern-angular-bootcamp/section-12-a-typescript-overview/prod/ts/index.ts
+++ udemy/the-modern-angular-bootcamp/section-12-a-typescript-overview/prod/ts/index.ts
@@ -9,3 +9,19 @@ let otherName = 'Marcelo';
 
 const sentence = 'This is a sentence';
 sentence.includes('is');
+
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+const joinStrings = (a: string, b: string): string => {
+  return a + b;
+};
+
+function add2(a: number, b: number): number {
+  return a + b;
+}
+
+const joinStrings2 = function (a: string, b: string): string {
+  return a + b;
+};
