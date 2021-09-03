2b6d78dd2c585d20428fd23b17121d6cd66517dc
Marcelo dos Santos <mdssjc@gmail.com>
2021-09-02 22:56:16 -0300

    feat: add Classes and Properties

--- udemy/the-modern-angular-bootcamp/section-12-a-typescript-overview/prod/ts/index.ts
+++ udemy/the-modern-angular-bootcamp/section-12-a-typescript-overview/prod/ts/index.ts
@@ -43,3 +43,21 @@ const printPost = (postToPrint: PostGatekeeper) => {
 };
 
 printPost(post);
+
+class Car {
+  color: string;
+  year: number;
+
+  constructor(color: string, year: number) {
+    this.color = color;
+    this.year = year;
+  }
+
+  drive() {
+    console.log('Vroom');
+  }
+}
+
+const myCar = new Car('red', 2000);
+myCar.drive();
+console.log(myCar.color, myCar.year);
