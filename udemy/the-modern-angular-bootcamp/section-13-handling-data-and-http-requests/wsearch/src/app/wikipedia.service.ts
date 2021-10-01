3977afe00798ce8ba33a4be06f8e8f10f8bd3879
Marcelo dos Santos <mdssjc@gmail.com>
2021-09-30 23:16:56 -0300

    feat: add Seeing the Request's Response

--- udemy/the-modern-angular-bootcamp/section-13-handling-data-and-http-requests/wsearch/src/app/wikipedia.service.ts
+++ udemy/the-modern-angular-bootcamp/section-13-handling-data-and-http-requests/wsearch/src/app/wikipedia.service.ts
@@ -7,6 +7,15 @@ export class WikipediaService {
   constructor(private http: HttpClient) {}
 
   public search(term: string) {
-    return this.http.get('');
+    return this.http.get('https://en.wikipedia.org/w/api.php', {
+      params: {
+        action: 'query',
+        format: 'json',
+        list: 'search',
+        utf8: '1',
+        srsearch: term,
+        origin: '*',
+      },
+    });
   }
 }
