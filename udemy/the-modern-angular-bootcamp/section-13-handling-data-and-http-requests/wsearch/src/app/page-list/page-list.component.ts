d15717664f09e39701b9cfc51abc2e061b2512a5
Marcelo dos Santos <mdssjc@gmail.com>
2021-09-30 23:27:09 -0300

    feat: add Building the Table

--- udemy/the-modern-angular-bootcamp/section-13-handling-data-and-http-requests/wsearch/src/app/page-list/page-list.component.ts
+++ udemy/the-modern-angular-bootcamp/section-13-handling-data-and-http-requests/wsearch/src/app/page-list/page-list.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./page-list.component.css'],
 })
 export class PageListComponent implements OnInit {
-  @Input() pages = [];
+  @Input() pages: any = [];
 
   constructor() {}
 
