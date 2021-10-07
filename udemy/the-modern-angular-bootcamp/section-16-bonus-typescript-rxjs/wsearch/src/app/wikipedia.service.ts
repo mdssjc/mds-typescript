ad53e7baa7e02f32c275dae363585f21cd8b8650
Marcelo dos Santos <mdssjc@gmail.com>
2021-10-06 23:01:12 -0300

    feat: add TypeScript is Smart

--- udemy/the-modern-angular-bootcamp/section-16-bonus-typescript-rxjs/wsearch/src/app/wikipedia.service.ts
+++ udemy/the-modern-angular-bootcamp/section-16-bonus-typescript-rxjs/wsearch/src/app/wikipedia.service.ts
@@ -2,6 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { pluck } from 'rxjs/operators';
 
+interface WikipediaResponse {
+  query: {
+    search: {
+      title: string;
+      snippet: string;
+      pageid: number;
+    }[];
+  };
+}
+
 @Injectable({
   providedIn: 'root',
 })
@@ -10,7 +20,7 @@ export class WikipediaService {
 
   public search(term: string) {
     return this.http
-      .get('https://en.wikipedia.org/w/api.php', {
+      .get<WikipediaResponse>('https://en.wikipedia.org/w/api.php', {
         params: {
           action: 'query',
           format: 'json',
