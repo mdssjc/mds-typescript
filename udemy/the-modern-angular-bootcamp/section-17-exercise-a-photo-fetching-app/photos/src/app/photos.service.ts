166080c012407b903455212b041bd9b344e4fa10
Marcelo dos Santos <mdssjc@gmail.com>
2021-10-11 22:54:20 -0300

    feat: add Using a Generic Type

--- udemy/the-modern-angular-bootcamp/section-17-exercise-a-photo-fetching-app/photos/src/app/photos.service.ts
+++ udemy/the-modern-angular-bootcamp/section-17-exercise-a-photo-fetching-app/photos/src/app/photos.service.ts
@@ -1,17 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+interface UnsplashResponse {
+  urls: {
+    regular: string;
+  };
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class PhotosService {
+  private appkey = '---';
+
   constructor(private http: HttpClient) {}
 
   getPhoto() {
-    return this.http.get('https://api.unsplash.com/photos/random', {
-      headers: {
-        Authorization: 'Client-ID ---',
-      },
-    });
+    return this.http.get<UnsplashResponse>(
+      'https://api.unsplash.com/photos/random',
+      {
+        headers: {
+          Authorization: 'Client-ID ' + this.appkey,
+        },
+      }
+    );
   }
 }
