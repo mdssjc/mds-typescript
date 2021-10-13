10597aaa2b1186df2e9a67c24e153c52f02ff38b
Marcelo dos Santos <mdssjc@gmail.com>
2021-10-12 22:48:02 -0300

    feat: add Changing Styling on Validation Errors

--- udemy/the-modern-angular-bootcamp/section-18-credit-card-payments-with-reactive-forms/creditcard/src/app/input/input.component.ts
+++ udemy/the-modern-angular-bootcamp/section-18-credit-card-payments-with-reactive-forms/creditcard/src/app/input/input.component.ts
@@ -8,8 +8,14 @@ import { FormControl } from '@angular/forms';
 })
 export class InputComponent implements OnInit {
   @Input() control: FormControl | any;
+  @Input() label: string | any;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  showErrors() {
+    const { dirty, touched, errors } = this.control;
+    return dirty && touched && errors;
+  }
 }
