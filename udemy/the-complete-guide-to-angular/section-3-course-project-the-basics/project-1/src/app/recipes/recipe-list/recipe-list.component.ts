29e759b48239070dc4bbd7a9b63e508a8b2b632c
Marcelo dos Santos <mdssjc@gmail.com>
2022-05-21 22:54:04 -0300

    feat: add Adding Content to the Recipes Components

--- udemy/the-complete-guide-to-angular/section-3-course-project-the-basics/project-1/src/app/recipes/recipe-list/recipe-list.component.ts
+++ udemy/the-complete-guide-to-angular/section-3-course-project-the-basics/project-1/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'This is simply a test',
+      'https://i0.wp.com/recipesbycarina.com/wp-content/uploads/2017/01/Fluffy-Pancakes-Recipe-square-scaled.jpg'
+    ),
+  ];
 
   constructor() {}
 
