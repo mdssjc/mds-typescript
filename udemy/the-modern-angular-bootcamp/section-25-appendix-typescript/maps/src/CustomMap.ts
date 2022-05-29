b589dcb689910697946df116cf4b4fa1db932696
Marcelo dos Santos <mdssjc@gmail.com>
2022-05-29 14:27:39 -0300

    feat: add Adding Markers

--- udemy/the-modern-angular-bootcamp/section-25-appendix-typescript/maps/src/CustomMap.ts
+++ udemy/the-modern-angular-bootcamp/section-25-appendix-typescript/maps/src/CustomMap.ts
@@ -1,10 +1,27 @@
+import { Company } from './Company';
+import { User } from './User';
+
 export class CustomMap {
   public googleMap: google.maps.Map;
 
   constructor(divId: string) {
     this.googleMap = new google.maps.Map(document.getElementById(divId), {
-      zoom: 11,
+      zoom: 1,
       center: { lat: 0, lng: 0 },
+    });
+  }
+
+  addUserMarker(user: User) {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: user.location,
+    });
+  }
+
+  addCompanyMarker(company: Company) {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: company.location,
     });
   }
 }
