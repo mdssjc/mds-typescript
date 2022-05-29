4e780e2cc921629e6ca9db8089b4027326fa4f54
Marcelo dos Santos <mdssjc@gmail.com>
2022-05-29 13:19:40 -0300

    feat: add Using Type Definition Files

--- udemy/the-modern-angular-bootcamp/section-25-appendix-typescript/maps/src/User.ts
+++ udemy/the-modern-angular-bootcamp/section-25-appendix-typescript/maps/src/User.ts
@@ -1,1 +1,17 @@
-class User {}
+import faker from 'faker';
+
+class User {
+  name: string;
+  localion: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.name.firstName();
+    this.localion = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+}
