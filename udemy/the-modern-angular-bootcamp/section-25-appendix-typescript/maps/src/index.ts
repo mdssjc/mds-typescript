a6ad26e5af5c9cdab3394bb83e49b6868fda28ce
Marcelo dos Santos <mdssjc@gmail.com>
2022-05-29 14:00:23 -0300

    feat: add Exploring Type Definition Files

--- udemy/the-modern-angular-bootcamp/section-25-appendix-typescript/maps/src/index.ts
+++ udemy/the-modern-angular-bootcamp/section-25-appendix-typescript/maps/src/index.ts
@@ -1,8 +1,11 @@
+/// <reference types="@types/google.maps" />
 import { Company } from './Company';
 import { User } from './User';
 
 const user = new User();
 const company = new Company();
 
-console.log(user);
-console.log(company);
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 13,
+  center: user.location,
+});
